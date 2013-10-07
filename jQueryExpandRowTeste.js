344f57980a02e4a04da6950a1b81539efbf80b2d
Renato Batista <sonomel00@yahoo.com.br>
2013-10-07 14:55:17 -0300

    Incluida funcionalidade para movimentar a linha master com as linhas detalhes para cima e para baixo conforme selecao em radiobutton

--- jQueryExpandRowTeste.js
+++ jQueryExpandRowTeste.js
@@ -1,10 +1,75 @@
 jQuery(document).ready(function(){
+	ativaFuncoesNaTabela();
+	
+	jQuery('.rowDetail').hide();
+});
+
+function movimentaLinhaAcima() {	
+	var linhaSelecionada = jQuery('input[type=radio]').filter(':checked');
+	
+	if(linhaSelecionada.length == 0) {
+		alert('Selecione alguém para movimentar sua anta');
+		return;
+	}
+	
+	var linhaMaster = linhaSelecionada.parents('tr');
+	var linhaMasterAnterior = linhaMaster.prevUntil('.rowHeader').last().prev();
+	
+	if(linhaMasterAnterior.length < 1) {
+		linhaMasterAnterior = linhaMaster.prev();
+	}
+	
+	if(linhaMasterAnterior.length > 0) {	
+		var linhaDetalhe = linhaMaster.nextUntil('.rowHeader');		
+		var conteudoParaMovimentar = jQuery.merge(linhaMaster, linhaDetalhe);
+		
+		conteudoParaMovimentar.remove();
+		conteudoParaMovimentar.insertBefore(linhaMasterAnterior);
+		
+		ativaFuncoesNaTabela();
+	}
+}
+
+function movimentaLinhaAbaixo() {
+	var linhaSelecionada = jQuery('input[type=radio').filter(':checked');
+	
+	if(linhaSelecionada.length == 0) {
+		alert('Selecione alguém para movimentar sua anta');
+	}
+	
+	var linhaMaster = linhaSelecionada.parents('tr');
+	var linhaDetalhe = linhaMaster.nextUntil('.rowHeader');
+	
+	if(linhaDetalhe.length > 0) {
+		var linhaMasterPosterior = linhaDetalhe.last().next();
+	} else {
+		var linhaMasterPosterior = linhaMaster.next();
+	}
+	
+	if(linhaMasterPosterior.length > 0) {
+		var conteudoParaMovimentar = jQuery.merge(linhaMaster, linhaDetalhe);
+		
+		var colocarDepoisDestaLinha = linhaMasterPosterior.nextUntil('.rowHeader').last();
+		
+		if(colocarDepoisDestaLinha.length < 1) {
+			colocarDepoisDestaLinha = linhaMasterPosterior;
+		}
+		
+		conteudoParaMovimentar.remove();
+		conteudoParaMovimentar.insertAfter(colocarDepoisDestaLinha);
+		
+		ativaFuncoesNaTabela();
+	}		
+}
+
+function ativaFuncoesNaTabela() {
 	jQuery('.rowHeader').each(function(){
 			var linhaAtual = jQuery(this);
-			var tdClicavel = linhaAtual.children().first();
+			var tdClicavel = linhaAtual.children().eq(1);
 			
-			tdClicavel.css('cursor', 'hand');			
-			tdClicavel.click(function(e) {
+			tdClicavel.css('cursor', 'hand');
+			tdClicavel.unbind('click');
+			tdClicavel.bind('click', function(e) {
 					var signalClick = jQuery(this);
 					e.preventDefault();
 					
@@ -17,6 +82,4 @@ jQuery(document).ready(function(){
 					}
 				});
 		});
-	
-	jQuery('.rowDetail').hide();
-});
+}
